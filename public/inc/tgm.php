868fb8bca170156a590c7b0366bbf1372f3747b7
Zonayed Ahmed <zonayedpca@yahoo.com>
2018-08-11 10:26:36 +0530

    type fix

--- public/inc/tgm.php
+++ public/inc/tgm.php
@@ -70,11 +70,11 @@ function my_theme_register_required_plugins() {
 			'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
 			'external_url'       => '', // If set, overrides default API URL and points to an external URL.
 			'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
-		), 
+		),
         array(
 			'name'               => 'REST API – Filter Fields', // The plugin name.
 			'slug'               => 'rest-api-filter-fields', // The plugin slug (typically the folder name).
-			'source'             => get_stylesheet_directory() . '/plugins/rest-api-fliter.zip', // The plugin source.
+			'source'             => get_stylesheet_directory() . '/plugins/rest-api-filter.zip', // The plugin source.
 			'required'           => true, // If false, the plugin is only 'recommended' instead of required.
 			'version'            => '1.0.7', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
 			'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
